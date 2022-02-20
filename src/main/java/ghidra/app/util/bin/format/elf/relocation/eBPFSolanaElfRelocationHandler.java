bf08830c0fc9e6aaf1a38ecb0505622efaf016da
Marius Guggenmos <MariusGuggenmos@gmail.com>
2022-02-20 22:26:28 +0100

    Fix resolving syscalls + remove unused code Also fixes relocations with base address not equal to zero

--- src/main/java/ghidra/app/util/bin/format/elf/relocation/eBPFSolanaElfRelocationHandler.java
+++ src/main/java/ghidra/app/util/bin/format/elf/relocation/eBPFSolanaElfRelocationHandler.java
@@ -10,6 +10,8 @@ import ghidra.program.model.listing.Program;
 import ghidra.program.model.mem.Memory;
 import ghidra.program.model.mem.MemoryAccessException;
 import ghidra.util.exception.NotFoundException;
+import ghidra.program.model.symbol.Symbol;
+import ghidra.program.model.symbol.SymbolTable;
 
 public class eBPFSolanaElfRelocationHandler extends ElfRelocationHandler {
 	/// Start of the program bits (text and ro segments) in the memory map
@@ -24,26 +26,6 @@ public class eBPFSolanaElfRelocationHandler extends ElfRelocationHandler {
 	@Override
 	public boolean canRelocate(ElfHeader elf) {
 		return elf.e_machine() == ElfConstants.EM_BPF;
-	}
-
-	private int hashSymbolName(CharBuffer name) {
-		int hash = 0;//MurmurHash3.murmurhash3_x86_32(name, 0, name.length(), 0);
-		return hash;
-	}
-
-	private int hashBpfFunction(long pc, String name) {
-		ByteBuffer buffer;
-		if (name == "entrypoint") {
-			buffer = ByteBuffer.allocate("entrypoint".length());
-			for (char c : "entrypoint".toCharArray()) {
-				buffer.putChar(c);
-			}
-		} else {
-			buffer = ByteBuffer.allocate(Long.BYTES);
-			buffer.putLong(pc);
-		}
-		buffer.rewind();
-		return hashSymbolName(buffer.asCharBuffer());
 	}
 
 	@Override
@@ -89,38 +71,18 @@ public class eBPFSolanaElfRelocationHandler extends ElfRelocationHandler {
 
 		long baseOffset = elfRelocationContext.getImageBaseWordAdjustmentOffset();
 		Address imm_offset = relocationAddress.add(4);
-		// markAsWarning(program, imm_offset, symbolName, "checkIt", elfRelocationContext.getLog());
+		// the types are (hopefully) implemented according to how the solana rBPF implementation does it
 		// R_BPF_64_64
 		if (type == 1) {			
-			elfRelocationContext.getLog().appendMsg(String.format("type 1 reloc"));
-			try {
-				//String sec_name = elfRelocationContext.relocationTable.getSectionToBeRelocated().getNameAsString();
-				//if (sec_name.toString().contains("debug")) {
-				//	return;
-				//}
+			value = symbolValue + addend;
 
-				// skip relocations in the debug section ???
-				// String map = Symbol.getNameAsString();				
-				// long val = Symbol.getValue();
-					
-				// SymbolTable table = program.getSymbolTable();
-				// Address mapAddr = table.getSymbol(val).getAddress();
-				value = symbolValue + addend;
-
-				Address refd_va = program.getAddressFactory().getDefaultAddressSpace().getAddress(memory.getInt(imm_offset));
-				Address refd_pa = refd_va.add(baseOffset);
-				value += refd_pa.getOffset();
-					
-				// do the actual relocation work
-				// docs that explains how it works
-				// https://www.kernel.org/doc/Documentation/bpf/llvm_reloc.rst
-				// value = mapAddr.getAddressableWordOffset();		
-				// Byte dst = memory.getByte(relocationAddress.add(0x1));
-				memory.setInt(imm_offset, (int)(value & 0xffffffff));			
-				memory.setInt(imm_offset.add(8), (int)(value >> 32));			
-				// memory.setByte(relocationAddress.add(0x1), (byte) (dst + 0x10));				
-				}
-				catch(NullPointerException e) {}
+			Address refd_va = program.getAddressFactory().getDefaultAddressSpace().getAddress(memory.getInt(imm_offset));
+			Address refd_pa = refd_va.add(baseOffset);
+			value += refd_pa.getOffset();
+				
+			// do the actual relocation work
+			memory.setInt(imm_offset, (int)(value & 0xffffffff));			
+			memory.setInt(imm_offset.add(8), (int)(value >> 32));					
 		}
 		// R_BPF_64_Relative
 		else if (type == 8) {
@@ -130,8 +92,9 @@ public class eBPFSolanaElfRelocationHandler extends ElfRelocationHandler {
 			Address refd_pa = refd_va.add(baseOffset);
 			value += refd_pa.getOffset();
 			ElfSectionHeader text_section = elf.getSection(".text");
-			// check if
-			if (text_section.getOffset() <= offset && offset <= text_section.getOffset() + text_section.getSize()) {
+			// check if split relocation across 2 instruction slots or single 64 bit value
+			long relativeOffset = offset - baseOffset;
+			if (text_section.getOffset() <= relativeOffset && relativeOffset <= text_section.getOffset() + text_section.getSize()) {
 				// write value split in two slots, high and slow
 				// elfRelocationContext.getLog().appendMsg(String.format("split set: %x = %x", imm_offset.getOffset(), value));
 				memory.setInt(imm_offset, (int)(value & 0xffffffff));
@@ -144,41 +107,37 @@ public class eBPFSolanaElfRelocationHandler extends ElfRelocationHandler {
 		}
 		// R_Bpf_64_32 = 10,
 		else if (type == 10) {
-			int hash;
+			int targetAddr;
 			String call_type;
-			// elfRelocationContext.getLog().appendMsg(String.format("type 10 reloc"));
+			// normally we would put the hash into the immediate field of
+			// the call but then we would have to resolve the call again
+			// in sleigh and I don't know how to do that
+			// therefore we just resolve the address relative to the current
+			// instruction and resolve it immediately :)
 			if (sym.isFunction() && symbolValue != 0) {
 				// bpf call
-				// ElfSectionHeader text_section = elf.getSection(".text");
 				long target_pc = symbolValue;// - text_section.getAddress();
-				// normally we would put the hash into the immediate field of
-				// the call but then we would have to resolve the call again
-				// in sleigh and I don't know how to do that
-				// therefore we just resolve the address relative to the current
-				// instruction and resolve it immediately :)
-				// // hash = hashBpfFunction(target_pc, symbolName);
-				// // current instruction address that the call will be relative to
-				long this_pc = relocation.getOffset() + 3 + 8 + baseOffset;
-				hash = (int)((target_pc - this_pc)/8);
-				// elfRelocationContext.getLog().appendMsg(String.format("this_pc: %x\ntarget_pc: %x\nhash: %x", this_pc, target_pc, hash));
+				
+				// next instruction address that the call will be relative to
+				long this_pc = relocation.getOffset() + 4 + baseOffset;
+				targetAddr = (int)((target_pc - this_pc)/8);
 				call_type = "function";
 			} else {
 				// syscall
-				CharBuffer buffer = CharBuffer.allocate(symbolName.length());
-				buffer.put(symbolName);
-				buffer.rewind();
-				hash = hashSymbolName(buffer);
-				// elfRelocationContext.getLog().appendMsg(String.format("type 10 (syscall) %s %x = %x, buf_pos: %d, len: %d, buffer: %s", symbolName, imm_offset.getOffset(), hash, buffer.position(), symbolName.length(),
-					// Arrays.toString(buffer.array())));
-				// buffer.charAt(buffer.position() -1)));
 				call_type = "syscall";
+				SymbolTable table = program.getSymbolTable();
+				Symbol symbol = table.getSymbol(symbolIndex);
+				// address of the symbol in the EXTERNAL section
+				long target_pc = symbol.getAddress().getOffset();
+				// next instruction address that the call will be relative to
+				long this_pc = relocation.getOffset() + 3 + baseOffset;
+				targetAddr = (int)((target_pc - this_pc)/8);
 			}
-			// elfRelocationContext.getLog().appendMsg(String.format("type 10 (isFunction %b) %s %x = %x", sym.isFunction() && symbolValue != 0, symbolName, imm_offset.getOffset(), hash));
-
-			memory.setInt(imm_offset, hash);
-			Listing listing = program.getListing();
-			listing.setComment(relocationAddress, CodeUnit.EOL_COMMENT,
-				String.format("%s_%s", call_type, symbolName));
+			
+			memory.setInt(imm_offset, targetAddr);
+			// Listing listing = program.getListing();
+			// listing.setComment(relocationAddress, CodeUnit.EOL_COMMENT,
+			//	String.format("%s_%s", call_type, symbolName));
 		} else {
 			appliedSymbol = false;
 		}
